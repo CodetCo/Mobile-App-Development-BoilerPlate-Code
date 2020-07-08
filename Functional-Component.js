a75c9817ebcf26f577a3a0f6923df395649d7523
Ashwin Chugh <32496343+AshwinChugh@users.noreply.github.com>
2020-07-07 22:43:27 -0700

    First Commit

---
+++ Functional-Component.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const componentName = (props) => (
+  <View style={styles.container}>
+    <Text>componentName</Text>
+  </View>
+);
+export default componentName;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
---
+++ README.md
@@ -0,0 +1,1 @@
+This is sample boilerplate code to copy and paste so you can get straight into writing your functional or class components/screens
